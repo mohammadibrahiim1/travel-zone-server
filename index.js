09dc248745bd0600544952d8d28daa7c3418f664
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-03-21 01:23:42 +0600

    updated data

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use("/api", ApiRouter);
 async function run() {
   const placesCollection = DB.client.db("travel-agency").collection("places");
   const reviewsCollection = DB.client.db("travel-agency").collection("reviews");
+
   const tourGuideCollection = DB.client
     .db("travel-agency")
     .collection("tourGuide");
@@ -46,8 +47,12 @@ async function run() {
   const paymentCollection = DB.client
     .db("travel-agency")
     .collection("payments");
-  const flightsCollection = DB.client.db("travel-agency").collection("flights");
+  // const flightsCollection = DB.client.db("travel-agency").collection("flights");
+  // const hotelsCollection = DB.client.db()
   try {
+    const hotelPlaceCollection = DB.client.db("travel-agency").collection("hotel-Country");
+    // console.log(hotelPlaceCollection);
+    const categoryCollection = DB.client.db("travel-agency").collection("category");
     app.get("/places", async (req, res) => {
       const query = {};
       const places = await placesCollection.find(query).toArray();
@@ -90,7 +95,13 @@ async function run() {
 
     app.get("/packages", async (req, res) => {
       const param = req.query;
-      if (!param.intFilter && !param.dmsFilter) {
+      if (
+        !param.intFilter &&
+        !param.dmsFilter &&
+        !param.tpFilter &&
+        !param.twpFilter &&
+        !param.thrFilter
+      ) {
         const data = await packagesCollection.find({}).toArray();
         return res.send(data);
       } else {
@@ -111,9 +122,35 @@ async function run() {
             },
           ];
         }
-        const filterData = await packagesCollection.find({
-          $or:filterQueries
-        }).toArray();
+        if (param.tpFilter) {
+          filterQueries = [
+            ...filterQueries,
+            {
+              offer: "10% discount",
+            },
+          ];
+        }
+        if (param.twpFilter) {
+          filterQueries = [
+            ...filterQueries,
+            {
+              offer: "20% discount",
+            },
+          ];
+        }
+        if (param.thrFilter) {
+          filterQueries = [
+            ...filterQueries,
+            {
+              offer: "30% discount",
+            },
+          ];
+        }
+        const filterData = await packagesCollection
+          .find({
+            $or: filterQueries,
+          })
+          .toArray();
         return res.send(filterData);
       }
     });
@@ -193,7 +230,170 @@ async function run() {
 
       // flight controller
       app.get("/flights", FlightController.show);
+
+     
+
+      // ================
     });
+
+
+
+
+
+
+
+
+ // ---------------hotel-bookings-------------
+
+ app.get("/hotelPlaces", async (req, res) => {
+  const query = {};
+  const result = await hotelPlaceCollection.find(query).toArray();
+  res.send(result);
+  
+});
+
+app.get("/category", async (req, res) => {
+  if (req.query.country) {
+    const query = { country: req.query.country };
+    const result = await categoryCollection.find(query).toArray();
+    res.send(result);
+  } else {
+    const query = {};
+    const result = await categoryCollection.find(query).toArray();
+    res.send(result);
+    console.log(result);
+  }
+});
+
+// ===============================
+
+app.get("/category/filter/v2", async (req, res) => {
+  const param = req.query;
+  if (
+    !param.brfFilter &&
+    !param.frIntFilter &&
+    !param.freeAirFilter &&
+    !param.airConFilter &&
+    !param.fitness &&
+    !param.pool
+  ) {
+    const data = await categoryCollection.find({}).toArray();
+    return res.send(data);
+  } else {
+    let filterQueries = [];
+    if (param.brfFilter) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          freeBreakFast: "Free breakfast",
+        },
+      ];
+    }
+
+    if (param.frIntFilter) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          freeInternet: "Free internet",
+        },
+      ];
+    }
+    if (param.freeAirFilter) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          freeAirportShuttle: "Free airport shuttle",
+        },
+      ];
+    }
+
+    if (param.airConFilter) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          airConditioned: "Air conditioned",
+        },
+      ];
+    }
+
+    if (param.fitness) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          fitness: "Fitness",
+        },
+      ];
+    }
+
+    if (param.pool) {
+      filterQueries = [
+        ...filterQueries,
+        {
+          pool: "Pool",
+        },
+      ];
+    }
+
+    const filterData = await categoryCollection
+      .find({ $or: filterQueries })
+      .toArray();
+    return res.send(filterData);
+  }
+});
+
+// ==========================================
+
+app.get("/category/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const booking = await categoryCollection.findOne(query);
+  res.send(booking);
+});
+
+app.get("/category/search/getHotelBySearch", async (req, res) => {
+  try {
+    const city = req.query.city;
+    const price = parseInt(req.query.price);
+    const room = parseInt(req.query.room);
+    const guests = parseInt(req.query.guests);
+
+    const hotels = await categoryCollection
+      .find({
+        $and: [
+          city ? { city } : {},
+          price ? { price: { $lte: price } } : {},
+          room ? { room: { $gte: room } } : {},
+          guests ? { guests: { $gte: guests } } : {},
+        ],
+      })
+      .toArray();
+
+    res.status(200).json({
+      success: true,
+      message: "Successful",
+      data: hotels,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "not found",
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   } finally {
   }
 }
