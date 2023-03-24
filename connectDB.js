0ab456dff8566f04f7f567bd401dccc1a014063a
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-03-24 16:45:38 +0600

    add env file

--- connectDB.js
+++ connectDB.js
@@ -1,22 +1,23 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri =
-    "mongodb+srv://user2:0kw4llp4OEF6BZGQ@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority";
+
+
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 exports.connectDB = async () => {
-    try {
-        await client.connect()
-        console.log('DataBase connection established successfully'.cyan.bold)
-    }
-    catch (err) { console.log(err.message.red.bold) }
-
-
-}
-exports.client = client
-
+  try {
+    await client.connect();
+    console.log("DataBase connection established successfully".cyan.bold);
+  } catch (err) {
+    console.log(err.message.red.bold);
+  }
+};
+exports.client = client;
